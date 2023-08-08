32ccdad80e93e5472200531ff2644b70d98d7f1a
canguejamba <canguejamba@gmail.com>
2023-08-08 19:10:26 +0200

    first commit and services modules

---
+++ author-api/src/main/java/io/cangue/authorapi/AuthorApiApplication.java
@@ -0,0 +1,13 @@
+package io.cangue.authorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorApiApplication.class, args);
+	}
+
+}
---
+++ book-api/src/test/java/io/cangue/bookapi/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cangue.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
