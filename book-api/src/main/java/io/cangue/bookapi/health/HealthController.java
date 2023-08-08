d948e69edc3cfc336da7e8477171d600fb4faf37
canguejamba <canguejamba@gmail.com>
2023-08-08 19:42:37 +0200

    setup api service projects and API Gateway

---
+++ book-api/src/main/java/io/cangue/bookapi/health/HealthController.java
@@ -0,0 +1,18 @@
+package io.cangue.bookapi.health;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = HealthController.BASE_URL)
+public class HealthController {
+    public static final String BASE_URL = "/health";
+
+    @GetMapping("")
+    public ResponseEntity<String> getHealth() {
+        return ResponseEntity.ok("Book API is up and running");
+    }
+
+}
